b4999d69e38911ecef4dcbaae22fd8ed336f9362
khaled39ab <khaled39ab@gmail.com>
2022-01-01 19:08:42 +0600

    day 2 challenge done

--- day2.js
+++ day2.js
@@ -1,3 +1,9 @@
+//Problem:
+// Write a program to reverse a string
+// String can be reversed by iterating it and storing it in reverse order
+// String can also be reversed by converting it to array, then joining it after reversing
+
+
 const str = "JavaScript is awesome"
 
 function reverseAString(str) {
