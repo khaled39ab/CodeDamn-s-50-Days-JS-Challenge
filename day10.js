e97b6846edbf1d7639f58b463b88f84cbe8cd6bc
khaled39ab <khaled39ab@gmail.com>
2022-01-03 18:46:28 +0600

    day 10 challenge done

---
+++ day10.js
@@ -0,0 +1,22 @@
+// Problem
+// Write a function to remove array element based on object property?
+// How to remove array element based on object property?
+// removeArrayElement("money") returns the array without the money object
+// removeArrayElement("id") returns the array without the id object
+// removeArrayElement("cStatus") returns the array without the cStatus object
+
+const array = [
+    { field: "id", operator: "eq" },
+    { field: "cStatus", operator: "eq" },
+    { field: "money", operator: "eq" },
+];
+
+const filterField = "money"
+
+function removeArrayElement(filterField) {
+    // write your solution here
+    const removePro = array.filter (item => item.field !== filterField)
+    return removePro;
+}
+
+console.log(`filtered array: ${removeArrayElement(filterField)}`)
